81c3c38aaf49ab1949fec20422335cb2febc3c88
jeevan916 <matrixjeevan@gmail.com>
2026-03-02 17:33:59 +0530

    build: Configure output directory to 'build'
    Updates Vite and Express configurations to use 'build' as the output directory for production assets. This standardizes the deployment target.

--- server.js
+++ server.js
@@ -577,9 +577,9 @@ async function startServer() {
     });
     app.use(vite.middlewares);
   } else {
-    app.use(express.static(path.join(__dirname, "dist")));
+    app.use(express.static(path.join(__dirname, "build")));
     app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "dist", "index.html"));
+      res.sendFile(path.join(__dirname, "build", "index.html"));
     });
   }
 
